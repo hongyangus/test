3167bfb15b5783e00dd3619a68b902ad49e2fb02
LHJ <s5689412@hotmail.com>
2018-09-05 23:48:09 +0800

    ECommerceContainer Add

---
+++ PropertyManagement/Models/Container.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PropertyManagement.Models
+{
+    public class ECommerceContainer
+    {
+        public int ContainerID;
+        public string ContainerName;
+        public int ContainerNumber;
+        public DateTime ShippedDate;
+        public DateTime EstimateArrivalDate;
+        public DateTime ArrivalDate;
+        public DateTime UnloadDate;
+        public DateTime MarketDate;
+        public string UnloadBy;
+        public float UnloadTimePeriod;
+        public string Notes;
+    }
+}
